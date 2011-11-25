2e994dd3a55bac1812f187474a311123648870da
Nuno Nunes <nuno@nunonunes.org>
2011-11-25 16:05:51 +0000

    These can't be loaded from the script, as they will be loaded from the configuration DB later on and cause a conflict.

--- script/brownian.pl
+++ script/brownian.pl
@@ -10,7 +10,6 @@ use lib join '/', File::Spec->splitdir(dirname(__FILE__)), '..', 'lib';
 use lib '/home/infobot/lib';
 
 use Bot::BasicBot::Pluggable;
-use Bot::BasicBot::Pluggable::Module::Brownian;
 
 my $brownian = Bot::BasicBot::Pluggable->new(
     channels => [ "#heartofgold" ],
@@ -21,10 +20,6 @@ my $brownian = Bot::BasicBot::Pluggable->new(
 
 $brownian->load("Loader");
 $brownian->load("Auth");
-$brownian->load("Seen");
-$brownian->load("Infobot");
-$brownian->load("Title");
-$brownian->load("Karma");
 
 $brownian->run();
 
