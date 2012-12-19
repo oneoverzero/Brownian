26c52773f340ccdc8849986ba546e9411c026ceb
Sérgio Bernardino <code@sergiobernardino.net>
2012-12-19 10:47:50 +0000

    Updated the 'links.pl' script to work with brownian's own log files.

--- scripts/links.pl
+++ scripts/links.pl
@@ -4,6 +4,7 @@ use 5.014;
 use warnings;
 
 use Regexp::Common qw /URI/;
+use File::Basename 'basename';
 use CGI;
 
 #
@@ -12,9 +13,27 @@ open my $fh, '<', $ARGV[0] or die $!;
 
 my $urls = {};
 
+my $found = 0;
+my $date;
+my $url;
 while (<$fh>)
 {
-  /^([-\d]+).*($RE{URI}{HTTP})/ && do { $urls->{$1}{$2}++ };
+  /^\[[#\w]+\s+([:\d]+)\].*($RE{URI}{HTTP}{ -scheme => qr{https?} })/ &&
+    do
+    {
+      $found++;
+      $date = $1;
+      $url  = $2;
+      $urls->{$date}{$url}++
+    };
+
+  $found && /brownian> \[ ([\w\W]+) \]/ &&
+    do
+    {
+      $found--;
+      $urls->{$date}{$url} = $1;
+      $date = $url = undef;
+    };
 }
 
 close $fh;
@@ -24,15 +43,15 @@ close $fh;
 my $cgi = CGI->new;
 
 say $cgi->start_html(-title => 'Links from #heartofgold');
-say $cgi->h1( 'Links from #heartofgold' );
+say $cgi->h1( 'Links from ' . $cgi->a( { href => basename $ARGV[0] }, basename $ARGV[0] ) );
 
 foreach my $date (sort {
-    join('', (split '-', $a)[2,1,0]) cmp join('', (split '-', $b)[2,1,0]);
+    join('', (split ':', $a)[0,1,2]) cmp join('', (split ':', $b)[0,1,2]);
   } keys $urls)
 {
 
   say $cgi->h2( $date );
-  say $cgi->ul( $cgi->li({-type=>'disc'}, [ sort map { $cgi->a( { href => $_ }, $_ ) } sort keys $urls->{$date} ]) );
+  say $cgi->ul( $cgi->li({-type=>'disc'}, [ sort map { "$urls->{$date}{$_}  : " . $cgi->a( { href => $_ }, $_ ) } sort keys $urls->{$date} ]) );
 }
 
 say $cgi->end_html;
